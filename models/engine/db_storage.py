cd2f83dcafacfa1946975a41671b8a61ca34a702
AndresBlandonW <andblandon@hotmail.com>
2021-12-22 19:20:26 -0800

    big file updates

---
+++ models/engine/db_storage.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""Database Storage"""
+from sqlalchemy import Column, Table
+from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.city import City
+from models.base_model import BaseModel, Base
+import os
+
+
+class DBStorage:
+    """ DataBase Storage class """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """ Time to make some shit """
+        from sqlalchemy import create_engine
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+                        os.getenv('HBNB_MYSQL_USER'),
+                        os.getenv('HBNB_MYSQL_PWD'),
+                        os.getenv('HBNB_MYSQL_HOST'),
+                        os.getenv('HBNB_MYSQL_DB'), pool_pre_ping=True))
+        if os.getenv('HBNB_ENV') == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ Doing all of something """
+        types = {
+                'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                'State': State, 'City': City, 'Amenity': Amenity,
+                'Review': Review
+            }
+        if cls is not None and cls != "":
+            the_type = types.get(cls)
+            result = self.__session.query(the_type).all()
+        else:
+            result = self.__session.query(State).all()
+            result = result + self.__session.query(City).all()
+            result = result + self.__session.query(User).all()
+            result = result + self.__session.query(Place).all()
+            result = result + self.__session.query(Review).all()
+            result = result + self.__session.query(Amenity).all()
+        return_dict = {}
+        for item in result:
+            key = item.__class__.__name__ + "." + item.id
+            value = item
+            return_dict.update({key: value})
+        return return_dict
+
+    def new(self, obj):
+        """add the object to the current database session"""
+        if obj is not None:
+            self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database"""
+        self.__session.commit()
+
+    def delete(self, obj):
+        """delete from the current database session"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """reload db"""
+        from sqlalchemy.orm import sessionmaker, scoped_session
+        Base.metadata.create_all(self.__engine)
+        sesh_thing = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sesh_thing)
+        self.__session = Session()
+
+    def close(self):
+        """close method"""
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        @property
+        def cities(self):
+            """Returns the list of City instances with state_id"""
+            from models.engine.file_storage import FileStorage
+            from models.city import City
+            files = FileStorage.all(City)
+            city_list = []
+            for key, value in files.items():
+                if 'City' in key and self.id == value.state_id:
+                    city_list.append(value)
+            return city_list

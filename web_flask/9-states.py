86b322b0b9201cc4214bf3f0c72898a2b8016060
Andres Blandon <andblandon@hotmail.com>
2022-01-31 04:59:18 +0000

    tk10

---
+++ web_flask/9-states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from models import storage
+from flask import Flask, abort, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """Renders an HTML page"""
+    states = storage.all("State")
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<_id>', strict_slashes=False)
+def state(_id):
+    """Renders an HTML page"""
+    states = storage.all("State")
+    state = None
+    for st in states.values():
+        if _id == st.id:
+            state = st
+            break
+    return render_template('9-states.html', state=state)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
